e7db667b7c01045de09639a45e25795b3e3d0e90
wakana <wakanawardrobe@gmail.com>
2023-09-13 15:09:59 +0100

    add ability for users to sign up and log in

--- app.js
+++ app.js
@@ -25,6 +25,43 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true}));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username"});
+      };
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password"});
+      }
+
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+// app.use((req, res, next) => {
+//   res.locals.currentUser = req.user;
+//   next();
+// })
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -32,7 +69,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res, next) => res.render("index"));
+app.get("/", (req, res, next) => res.render("index", { user: req.user }));
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
@@ -53,9 +90,24 @@ app.post("/sign-up", async (req, res, next) => {
     return next(err);
   }
 
-})
+});
 
-// app.use("/", indexRouter);
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
 
 app.use(function(err, req, res, next) {
 
